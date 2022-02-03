5d333e91e2a2e83f9a5b7860e486d9afb65f1b85
Roberto <mario24roberto@gmail.com>
2022-02-02 19:23:56 -0600

    Agregando la negacion

---
+++ 01-fundamentos/assets/js/loga-booleana.js
@@ -0,0 +1,11 @@
+const regresaTrue= ()=>{
+    console.log("Regresa True")
+    return true;
+}
+const regresaFalse= ()=>{
+    console.log("Regresa False")
+    return false;
+}
+console.warn("Not o la negacion")
+console.log(true)
+console.log(!true)
